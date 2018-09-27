919c6ce1628e5817c3387901cd332bcffcdd4fa4
Ashley Singleton <asingleton6@gmail.com>
2018-09-26 22:41:11 -0400

    Added video funtionality and styling for the story and coloring book.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,47 @@
-let video = document.querySelector(".storybook-video");
-let device = document.querySelector(".device-container");
+let missionVideo = document.querySelector(".missionbook-video");
+let missiondevice = document.querySelector(".mission-device");
 
-function toggleVideo() {
-  if (video.paused) {
-    video.play()
-    video.classList.toggle("display-none");
-    device.style.transform = "rotate(90deg)";
-  } else if (video.play) {
-    video.pause()
-    video.classList.toggle("display-none");
-    device.style.transform = "rotate(0deg)";
+function toggleMissionVideo() {
+  if (missionVideo.paused) {
+    missionVideo.play()
+    missionVideo.classList.toggle("display-none");
+    missiondevice.style.transform = "rotate(90deg)";
+  } else if (missionVideo.play) {
+    missionVideo.pause()
+    missionVideo.classList.toggle("display-none");
+    missiondevice.style.transform = "rotate(0deg)";
+
+  }
+}
+
+let storyVideo = document.querySelector(".storybook-video");
+let storydevice = document.querySelector(".story-device");
+
+function toggleStoryVideo() {
+  if (storyVideo.paused) {
+    storyVideo.play()
+    storyVideo.classList.toggle("display-none");
+    storydevice.style.transform = "rotate(90deg)";
+  } else if (storyVideo.play) {
+    storyVideo.pause()
+    storyVideo.classList.toggle("display-none");
+    storydevice.style.transform = "rotate(0deg)";
+
+  }
+}
+
+let coloringVideo = document.querySelector(".coloringbook-video");
+let coloringdevice = document.querySelector(".coloring-device");
+
+function toggleColoringVideo() {
+  if (coloringVideo.paused) {
+    coloringVideo.play()
+    coloringVideo.classList.toggle("display-none");
+    coloringdevice.style.transform = "rotate(90deg)";
+  } else if (coloringVideo.play) {
+    coloringVideo.pause()
+    coloringVideo.classList.toggle("display-none");
+    coloringdevice.style.transform = "rotate(0deg)";
 
   }
 }
